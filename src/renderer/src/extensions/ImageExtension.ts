43e45643c5351c6c09f4c7cdf9e8f0c035e76c3c
Coderbeep <jakubkubiak234@gmail.com>
2025-01-24 11:19:22 +0100

    FEATURE: Image extension

--- src/renderer/src/extensions/ImageExtension.ts
+++ src/renderer/src/extensions/ImageExtension.ts
@@ -2,6 +2,8 @@ import { syntaxTree } from '@codemirror/language';
 import { RangeSetBuilder, StateEffect, StateField } from "@codemirror/state";
 import { Decoration, DecorationSet, EditorView, WidgetType } from "@codemirror/view";
 import contextMenuManager, { ContextMenuType } from '@renderer/components/ContextMenuManager';
+import { notesDirectoryPath } from '@shared/constants';
+import { doesPathExist } from '../../utils/db';
 
 // TODO: Vertical scrolling does not work properly
 // TODO: OnClick event for the image widget
@@ -12,75 +14,95 @@ import contextMenuManager, { ContextMenuType } from '@renderer/components/Contex
 const toggleImageEffect = StateEffect.define<DecorationSet>();
 
 const imageField = StateField.define({
-    create() {
-        return Decoration.none;
-    },
-    update(decorations, transaction) {
-        for (let effect of transaction.effects) {
-            if (effect.is(toggleImageEffect)) {
-                decorations = effect.value;
-            }
-        }
-        return decorations;
-    },
-    provide: field => EditorView.decorations.from(field)
+  create() {
+    return Decoration.none;
+  },
+  update(decorations, transaction) {
+    for (let effect of transaction.effects) {
+      if (effect.is(toggleImageEffect)) {
+        decorations = effect.value;
+      }
+    }
+    return decorations;
+  },
+  provide: field => EditorView.decorations.from(field)
 });
 
+function checkImageExists(src) {
+  return doesPathExist(notesDirectoryPath + src).then((exists) => {
+    return exists;
+  })
+}
+
 function createImageDecorations(view) {
-    const builder = new RangeSetBuilder();
-    const { from, to } = view.state.selection.main;
+  const builder = new RangeSetBuilder();
+  const { from, to } = view.state.selection.main;
 
-    syntaxTree(view.state).iterate({
-        enter: (node) => { 
-            if (node.name === 'Image') {
-              const isActive = from >= node.from && to <= node.to;
-              if (!isActive) {
-                builder.add(node.from, node.to, Decoration.replace({
-                  block: true,
-                }));
-              }
-
-              builder.add(node.to, node.to, Decoration.widget({
-                widget: new ImageWidget(),
-                block: true,
-                side: 1
-            }));
-          }
+  syntaxTree(view.state).iterate({
+    enter: (node) => {
+      if (node.name === 'Image') {
+        const isActive = from >= node.from && to <= node.to;
+        const src = view.state.doc.sliceString(node.from + 3, node.to - 2);
+        
+        if (!isActive) {
+          builder.add(node.from, node.to, Decoration.replace({
+            block: true,
+          }));
         }
-    });
 
-    return builder.finish();
+        builder.add(node.to, node.to, Decoration.widget({
+          widget: new ImageWidget(src),
+          block: true,
+          side: 1
+        }));
+      }
+    }
+  });
+
+  return builder.finish();
 }
 
 function toggleImageVisibility(view) {
-    view.dispatch({
-        effects: toggleImageEffect.of(createImageDecorations(view))
-    });
+  view.dispatch({
+    effects: toggleImageEffect.of(createImageDecorations(view))
+  });
 }
 
 class ImageWidget extends WidgetType {
-  constructor() {
+  static cache = new Map();
+
+  constructor(private readonly src: string) {
     super();
   }
 
   toDOM(view) {
     const container = document.createElement('div');
-    container.className = 'cm-image-widget';
+    container.className = 'cm-image-widget cm-image-error';
+    container.innerHTML = `Image '${this.src}' not found`;
 
-    const img = document.createElement('img');
-    img.src = 'https://commonmark.org/help/images/favicon.png';
+    checkImageExists(this.src).then((exists) => {
+      if (exists) {
+        container.innerHTML = '';
+        container.className = 'cm-image-widget';
+        const img = document.createElement('img');
+        img.src = `media://${this.src}`;
+        container.appendChild(img);
+      }
+    })
 
-    container.appendChild(img);
-
-    container.addEventListener('contextmenu', (event) => {
-      event.preventDefault();
-      // TODO: Implement menu logic here
-      const { clientX: x, clientY: y } = event;
-      contextMenuManager.showMenu(ContextMenuType.Image, { x, y });
-      console.log('Right click on image widget');
-    });
+    // container.addEventListener('contextmenu', (event) => {
+    //   event.preventDefault();
+    //   // TODO: Implement menu logic here
+    //   const { clientX: x, clientY: y } = event;
+    //   contextMenuManager.showMenu(ContextMenuType.Image, { x, y });
+    //   console.log('Right click on image widget');
+    // });
 
     return container;
+  }
+
+  eq(other: ImageWidget) {
+    return this.src === other.src;
   }
 
   destroy(dom) {
@@ -88,16 +110,15 @@ class ImageWidget extends WidgetType {
   }
 
   get estimatedHeight() {
-    return 144;
+    return 137;
   }
 }
 
 export const ImageExtension = [
-    imageField,
-    EditorView.updateListener.of((update) => {
-        if (update.docChanged || update.selectionSet) {
-            toggleImageVisibility(update.view);
-        }
-        
-    })
+  imageField,
+  EditorView.updateListener.of((update) => {
+    if (update.docChanged || update.selectionSet) {
+      toggleImageVisibility(update.view);
+    }
+  })
 ];
