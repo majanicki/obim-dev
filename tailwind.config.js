368f5341d13654b72a3332017175abdc2eb4ad0f
Coderbeep <jakubkubiak234@gmail.com>
2025-01-28 16:33:45 +0100

    STYLE: breadcrumbs prepare, font color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/renderer/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        normal: '#222222',
+        muted: '#5C5C5C',
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')]
 }
