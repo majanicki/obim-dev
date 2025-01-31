d4d96e024c9507961fe217b2a824e01aa51bfd8a
Coderbeep <jakubkubiak234@gmail.com>
2025-01-31 13:43:03 +0100

    FEATURE: breadcrumbs and FE integration

--- src/renderer/src/components/Breadcrumbs.tsx
+++ src/renderer/src/components/Breadcrumbs.tsx
@@ -1,9 +1,11 @@
-import { currentFilePathAtom, currentRelativeFilePathAtom } from "@renderer/store/notes"
-import { useAtom, useAtomValue } from "jotai"
-import React from "react"
+import { currentRelativeFilePathAtom, selectedBreadcrumbAtom } from "@renderer/store/notes"
+import { useAtomValue, useSetAtom } from "jotai"
+import { Fragment, useRef } from "react"
 
-const BreadcrumbItem = ({ children, className }) => {
-    return <div className={`px-1 rounded-md select-none ${className}`}>
+const BreadcrumbItem = ({ children, className, onClick }) => {
+
+
+    return <div className={`px-1 rounded-md select-none ${className}`} onClick={onClick}>
         {children}
     </div>
 }
@@ -17,15 +19,33 @@ const BreadcrumbSeparator = () => {
 export const Breadcrumbs = () => {
     const path = useAtomValue(currentRelativeFilePathAtom)
     const parts = path.split('/').filter(part => part !== '')
+    const setSelectedBreadcrumb = useSetAtom(selectedBreadcrumbAtom)
+    const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null)
+
+    const handleClick = (index: number) => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current)
+        }
+
+        setSelectedBreadcrumb("/" + parts.slice(0, index + 1).join('/'))
+
+        timeoutRef.current = setTimeout(() => {
+            setSelectedBreadcrumb('')
+            timeoutRef.current = null
+        }, 300)
+    }
 
     return <div className="flex justify-center text-normal text-sm text-nowrap">
         {parts.map((part, index) => (
-            <>
-                <BreadcrumbItem className={index === parts.length - 1 ? "text-normal" : "text-muted hover:bg-gray-200 hover:text-normal" }>
-                    {part}  
+            <Fragment key={`${part}-${index}`}>
+                <BreadcrumbItem
+                    className={index === parts.length - 1 ? "text-normal"
+                            : "text-muted hover:bg-gray-200 hover:text-normal"}
+                    onClick={() => index < parts.length - 1 && handleClick(index)}>
+                    {part}
                 </BreadcrumbItem>
                 {index < parts.length - 1 && <BreadcrumbSeparator />}
-            </>
+            </Fragment>
         ))}
     </div>
 }
